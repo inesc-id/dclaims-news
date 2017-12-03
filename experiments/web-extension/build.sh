d4134cbef8811c26c89d7c5495af1f6df0a07394
Joao Santos <jrmsantos15@gmail.com>
2017-12-03 17:58:32 +0000

    experiments cleanup

--- experiments/web-extension/build.sh
+++
@@ -1,2 +0,0 @@
-./node_modules/.bin/webpack --config webpack.config.js
-cp /Users/boss/git/hypercerts/hypercerts/experiments/web-extension/dist/js_hypercerts/*.js /Users/boss/git/hypercerts/hypercerts/experiments/web-extension/dist/campus-community/tecnico-researcher-honoured-by-the-royal-society-of-chemistry/js_hypercerts
--- experiments/webpack-demo/src/index-2.js
+++
@@ -1,6 +0,0 @@
-function component2() {
-  var element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
-      document.body.appendChild(component());
